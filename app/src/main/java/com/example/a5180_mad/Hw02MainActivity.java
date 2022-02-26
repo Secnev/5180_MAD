c925013676be3b023e7234c7af040d071f97a1a9
David Vences <davidvences@Davids-MBP.attlocal.net>
2022-02-25 20:01:48 -0500

    Added HW02MainActivity and its layout

---
+++ app/src/main/java/com/example/a5180_mad/Hw02MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a5180_mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hw02MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hw02_main);
+    }
+}
